c0de33aa4904daae2cd595ba4835f6b59691f282
Kushagra Singh <kushagra.singh0602@gmail.com>
2025-10-17 09:02:24 +0530

    Change response status from 404 to 200

--- api/index.py
+++ api/index.py
@@ -34,5 +34,7 @@ class handler(BaseHTTPRequestHandler):
                 self.end_headers()
                 self.wfile.write(b'<!DOCTYPE html><html><body>Error</body></html>')
         else:
-            self.send_response(404)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
+            self.wfile.write(b'<!DOCTYPE html><html><body><h1>Unsubscribe Service</h1><p>Add ?email=your@email.com to unsubscribe</p></body></html>')
